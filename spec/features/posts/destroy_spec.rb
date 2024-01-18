1fd10592fa5e959567a594b47cd92085ff71e741
Kuper <killkih@mail.ru>
2024-01-18 16:30:21 +0300

    Posts #delete

---
+++ spec/features/posts/destroy_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'Author can destroy his post', "
+  In order not to clutter up the list
+  As an author
+  I'd like to be able to destroy my post
+" do
+
+  given!(:post) { create(:post) }
+
+  scenario 'Author tries to destroy his post' do
+
+    visit root_path
+    click_on 'Delete'
+
+    expect(page).to have_content 'Post successfully deleted!'
+  end
+end
