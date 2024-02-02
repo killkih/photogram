aa15683a28727e11787f8a6be49eb466899a6a3a
Kuper <killkih@mail.ru>
2024-02-02 14:18:38 +0300

    model + migration

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@
 class Post < ApplicationRecord
   belongs_to :user
 
+  has_many :comments, dependent: :destroy
+
   validates :content, presence: true
 
   scope :sort_by_date, -> { order(created_at: :desc) }
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it { should validate_presence_of :content }
+  it { should belong_to (:user) }
 
-  it { should belong_to(:user) }
+  it { should have_many(:comments).dependent(:destroy) }
+
+  it { should validate_presence_of :content }
 end
